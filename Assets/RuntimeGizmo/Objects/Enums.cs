fc376b098b673471180b75eff8f2b6114974e745
HiddenMonk <HiddenMonk@users.noreply.github.com>
2018-07-15 20:10:32 -0400

    Adding pivot center mode

--- Assets/RuntimeGizmo/Objects/Enums.cs
+++ Assets/RuntimeGizmo/Objects/Enums.cs
@@ -2,5 +2,6 @@
 {
 	public enum TransformSpace {Global, Local}
 	public enum TransformType {Move, Rotate, Scale}
+	public enum TransformPivot {Pivot, Center}
 	public enum Axis {None, X, Y, Z, Any}
 }
