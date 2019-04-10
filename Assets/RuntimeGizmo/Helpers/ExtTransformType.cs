4a729a8658d648752c7d3b92040e38ff25337faf
HiddenMonk <HiddenMonk@users.noreply.github.com>
2019-04-10 17:35:55 -0400

    Update ExtTransformType.cs

--- Assets/RuntimeGizmo/Helpers/ExtTransformType.cs
+++ Assets/RuntimeGizmo/Helpers/ExtTransformType.cs
@@ -6,18 +6,17 @@ namespace RuntimeGizmos
 	{
 		public static bool TransformTypeContains(this TransformType mainType, TransformType type, TransformSpace space)
 		{
+			if(type == mainType) return true;
+
 			if(mainType == TransformType.All)
 			{
 				if(type == TransformType.Move) return true;
 				else if(type == TransformType.Rotate) return true;
 				//else if(type == TransformType.RectTool) return false;
 				else if(type == TransformType.Scale && space == TransformSpace.Local) return true;
-				else return false;
 			}
-			else
-			{
-				return mainType == type;
-			}
+
+			return false;
 		}
 	}
 }
